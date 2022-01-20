f495cc1435747098b2b3c8dc06a44af18eccc5cb
Akira <85038692+Kazu-exe@users.noreply.github.com>
2022-01-21 00:09:52 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -8,7 +8,7 @@ AddEventHandler('akira:locationpay', function(price)
         
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(source)
-    local money = xPlayer.getInventoryItem('money')["count"]
+    local money = xPlayer.getMoney()
     if money >= price then
          xPlayer.removeMoney(price)
         TriggerClientEvent('esx:showNotification', source, "~g~Vous venez de louer un véhicule pour ".. price .." $")
