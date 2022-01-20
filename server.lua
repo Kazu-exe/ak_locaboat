366ae9ffcfa9577da77d850be68b8c8581e385f4
Akira <85038692+Kazu-exe@users.noreply.github.com>
2022-01-21 00:08:29 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,13 +2,15 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
+
 RegisterServerEvent('akira:locationpay')
 AddEventHandler('akira:locationpay', function(price)
+        
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(source)
     local money = xPlayer.getInventoryItem('money')["count"]
     if money >= price then
-        xPlayer.removeInventoryItem("money", price)
+         xPlayer.removeMoney(price)
         TriggerClientEvent('esx:showNotification', source, "~g~Vous venez de louer un véhicule pour ".. price .." $")
     end
 end)
